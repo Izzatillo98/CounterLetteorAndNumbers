ca070a03827c242b4992991ce07c5fccd56f255c
Izzatillo Abdusamatov <abdusamatovtillo8@gmail.com>
2024-07-06 09:38:05 +0500

    Add project files.

---
+++ CounterLetteorAndNumbers/Program.cs
@@ -0,0 +1,38 @@
+﻿using CounterLetteorAndNumber.Service;
+
+namespace CounterLetteorAndNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string isContinue;
+            do
+            {
+                Calculate();
+
+                Finder("Do you want continue? (" + ")");
+                isContinue = Console.ReadLine();
+
+            } 
+            while (isContinue == "+");
+        }
+              private static void Calculate()
+              {
+                   Finder("The are your text \n");
+                   Console.Write("Write the text : \n");
+
+                   string text = Console.ReadLine();
+
+                  Finder("The text have Letter and Number:");
+                  Finder($"Counter number of letters: {CounterServices.GenerateDigits(text)}");
+                  Finder($"Counter number of numbers: {CounterServices.GenerateLetters(text)}");
+
+              }
+              static void Finder(string text)
+              {
+                 Console.WriteLine(text);
+              }
+         
+    }
+}
---
+++ CounterLetteorAndNumbers/Service/CounterServices.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CounterLetteorAndNumber.Service
+{
+    internal class CounterServices
+    {
+        public static int GenerateDigits(string text)
+
+        {
+            int count = 0;
+            foreach (char c in text)
+            {
+                if (char.IsDigit(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static int GenerateLetters(string text)
+
+        {
+            int count = 0;
+            foreach (char c in text)
+            {
+                if (char.IsLetter(c))
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
